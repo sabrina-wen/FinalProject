adb270a3539764cb9e89825fb45d1ebacae42f3c
Peter Lee <dlee14@stuy.edu>
2017-01-08 23:52:17 -0500

    added testing button

--- WYSIWYG.java
+++ WYSIWYG.java
@@ -7,49 +7,66 @@ import java.awt.event.*;        //for action events
 import java.net.URL;
 import java.io.IOException;
 
-public class WYSIWYG extends JFrame {
-    private Container pane;
+public class WYSIWYG extends JFrame implements ActionListener {
+  private Container pane;
+  private JLabel uilabel;
 
-    public WYSIWYG () {
-	this.setTitle("WYSIWYG Editor");
-	this.setSize(800,600);
-	this.setLocation(100,100);
-	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+  public WYSIWYG () {
+    this.setTitle("WYSIWYG Editor");
+    this.setSize(800,600);
+    this.setLocation(100,100);
+    this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-	// initializing pane
-	pane = this.getContentPane();
-	pane.setLayout(new FlowLayout());
+    // initializing pane
+    pane = this.getContentPane();
+    pane.setLayout(new FlowLayout());
 
-	// lableing ui pane
-	JLabel uilabel = new JLabel("Type and format code here.", null, JLabel.LEFT);
-	uilabel.setPreferredSize(new Dimension(300, 30));
+    // lableing ui pane
+    uilabel = new JLabel("Type and format code here.", null, JLabel.LEFT);
+    uilabel.setPreferredSize(new Dimension(300, 30));
 
-	// labeling html code viewer
-	JLabel htmllabel = new JLabel("This is your HTML code! :o", null, JLabel.LEFT);
-     	htmllabel.setPreferredSize(new Dimension(300, 30));
-	
-	// creating ui pane
-	JTextPane ui = new JTextPane();
-	ui.setText( "dis b a jtextpane, the other is jeditor pane" );
-	JScrollPane uiscroll = new JScrollPane(ui);
-	uiscroll.setPreferredSize(new Dimension(300, 500));
+    // labeling html code viewer
+    JLabel htmllabel = new JLabel("This is your HTML code! :o", null, JLabel.LEFT);
+    htmllabel.setPreferredSize(new Dimension(300, 30));
 
-	// creating html code viewer pan
-	JEditorPane htmldisplay = new JEditorPane();
-       	JScrollPane htmlscroll = new JScrollPane(htmldisplay);
-	htmlscroll.setPreferredSize(new Dimension(300, 500));
+    // creating convert button for testing
+    JButton convert = new JButton("Convert");
+    convert.setActionCommand("convert");
+    convert.addActionListener(this);
 
-	// adding elements
-	pane.add(uilabel);
-	pane.add(htmllabel);
-	pane.add(uiscroll);
-	pane.add(htmlscroll);
+    // creating ui pane
+    JTextPane ui = new JTextPane();
+    ui.setText( "dis b a jtextpane, the other is jeditor pane" );
+    JScrollPane uiscroll = new JScrollPane(ui);
+    uiscroll.setPreferredSize(new Dimension(300, 500));
+
+    // creating html code viewer pan
+    JEditorPane htmldisplay = new JEditorPane();
+    JScrollPane htmlscroll = new JScrollPane(htmldisplay);
+    htmlscroll.setPreferredSize(new Dimension(300, 500));
+
+    // adding elements
+    pane.add(uilabel);
+    pane.add(htmllabel);
+    pane.add(uiscroll);
+    pane.add(convert);
+    pane.add(htmlscroll);
+  }
+
+  public void actionPerformed(ActionEvent e) {
+      if (e.getActionCommand().equals("convert")) {
+        uilabel.setText("what");
+        if ()
+      }
     }
 
-    public static void main (String[] args) {
-	WYSIWYG editor = new WYSIWYG();
-	editor.setVisible(true);
-    }
+
+
+
+
+
+  public static void main (String[] args) {
+    WYSIWYG editor = new WYSIWYG();
+    editor.setVisible(true);
+  }
 }
-	
-       
