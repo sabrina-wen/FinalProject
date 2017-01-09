b47b11969ba76c149d65b5634fa179acd34f60bf
sabrina-wen <sabrinawn253@gmail.com>
2017-01-09 14:40:30 -0500

    fixing up more git issues

--- WYSIWYG.java
+++ WYSIWYG.java
@@ -1,38 +1,11 @@
 import javax.swing.*;
 import javax.swing.text.*;
-<<<<<<< HEAD
 
 import java.awt.*;              //for layout managers and more
 import java.awt.event.*;        //for action events
 
 import java.net.URL;
 import java.io.IOException;
-
-public class WYSIWYG extends JFrame implements ActionListener {
-  private Container pane;
-  private JLabel uilabel;
-  private JTextComponent ui;
-
-  public WYSIWYG () {
-    this.setTitle("WYSIWYG Editor");
-    this.setSize(800,600);
-    this.setLocation(100,100);
-    this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-
-    // initializing pane
-    pane = this.getContentPane();
-    pane.setLayout(new FlowLayout());
-
-    // lableing ui pane
-    uilabel = new JLabel("Type and format code here.", null, JLabel.LEFT);
-    uilabel.setPreferredSize(new Dimension(300, 30));
-
-    // labeling html code viewer
-    JLabel htmllabel = new JLabel("This is your HTML code! :o", null, JLabel.LEFT);
-    htmllabel.setPreferredSize(new Dimension(300, 30));
-=======
-import java.awt.*;
-import java.awt.event.*;
 
 public class WYSIWYG extends JFrame {
     private Container pane;
@@ -66,14 +39,6 @@ public class WYSIWYG extends JFrame {
        	JScrollPane htmlscroll = new JScrollPane(htmldisplay);
 	htmlscroll.setPreferredSize(new Dimension(300, 500));
 
-	// adding elements
-	pane.add(uilabel);
-	pane.add(htmllabel);
-	pane.add(uiscroll);
-	pane.add(htmlscroll);
-    }
->>>>>>> cc0df5d89de8cce8eb17e03f9f9c4e74f95ba69e
-
     // creating convert button for testing
     JButton convert = new JButton("Convert");
     convert.setActionCommand("convert");
@@ -81,17 +46,6 @@ public class WYSIWYG extends JFrame {
     JButton bold = new JButton("Bold");
     bold.setActionCommand("bold");
     convert.addActionListener(this);
-
-    // creating ui pane
-    ui = new JTextPane();
-    ui.setText( "dis b a jtextpane, the other is jeditor pane" );
-    JScrollPane uiscroll = new JScrollPane(ui);
-    uiscroll.setPreferredSize(new Dimension(300, 500));
-
-    // creating html code viewer pan
-    JEditorPane htmldisplay = new JEditorPane();
-    JScrollPane htmlscroll = new JScrollPane(htmldisplay);
-    htmlscroll.setPreferredSize(new Dimension(300, 500));
 
     // adding elements
     pane.add(uilabel);
@@ -110,13 +64,10 @@ public class WYSIWYG extends JFrame {
         ui.replaceSelection(replacement);
       }
     }
-<<<<<<< HEAD
-
 
   public static void main (String[] args) {
     WYSIWYG editor = new WYSIWYG();
     editor.setVisible(true);
   }
-=======
->>>>>>> cc0df5d89de8cce8eb17e03f9f9c4e74f95ba69e
+
 }
