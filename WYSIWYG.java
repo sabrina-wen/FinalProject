3da505f18b23678f7da2fedf26c6a0f14f838ff2
Peter Lee <dlee14@stuy.edu>
2017-01-10 00:22:05 -0500

    Added StyleEditorKit

--- WYSIWYG.java
+++ WYSIWYG.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import javax.swing.text.*;
+import javax.swing.text.html.*;
 
 import java.awt.*;              //for layout managers and more
 import java.awt.event.*;        //for action events
@@ -7,10 +8,9 @@ import java.awt.event.*;        //for action events
 import java.net.URL;
 import java.io.IOException;
 
-public class WYSIWYG extends JFrame implements ActionListener{
+public class WYSIWYG extends JFrame {
   private Container pane;
   private JTextPane ui;
-  private StyledDocument document;
 
   public WYSIWYG () {
     this.setTitle("WYSIWYG Editor");
@@ -33,60 +33,37 @@ public class WYSIWYG extends JFrame implements ActionListener{
     // creating ui pane
     ui = new JTextPane();
     ui.setContentType("text/html");
-    ui.setText( "dis b a jtextpane, the other is jeditor pane" );
+    ui.setEditorKit(new HTMLEditorKit());
+    ui.setText( "dis b a <strong>jtextpane</strong>, the other is jeditor pane" );
     JScrollPane uiscroll = new JScrollPane(ui);
-    uiscroll.setPreferredSize(new Dimension(300, 500));
+    uiscroll.setPreferredSize(new Dimension(600, 500));
 
-    // // creating styledDocument
-    // document = ui.getStyledDocument();
-    // Style style = ui.addStyle("bold", null);
-    // StyleConstants.setBold(style, true);
+    uiscroll.setBorder(
+            BorderFactory.createCompoundBorder(
+                            BorderFactory.createTitledBorder("Text Fields"),
+                            BorderFactory.createEmptyBorder(1,1,1,1)));
+
+    JToolBar bar = new JToolBar();
+    bar.add(new StyledEditorKit.BoldAction());
+    bar.add(new StyledEditorKit.ItalicAction());
+    bar.add(new StyledEditorKit.UnderlineAction());
+    bar.add(new StyledEditorKit.AlignmentAction("Left", -1));
+    bar.add(new StyledEditorKit.AlignmentAction("Center", 1));
+    bar.add(new StyledEditorKit.AlignmentAction("Right", 2));
+    bar.add(new StyledEditorKit.FontSizeAction("14", 14));
+    bar.add(new StyledEditorKit.FontSizeAction("16", 16));
 
     // creating html code viewer pan
-    JEditorPane htmldisplay = new JEditorPane();
-    JScrollPane htmlscroll = new JScrollPane(htmldisplay);
-    htmlscroll.setPreferredSize(new Dimension(300, 500));
-
-    // creating convert button for testing
-    JButton bold = new JButton("Bold");
-    bold.setActionCommand("bold");
-    bold.addActionListener(this);
+    //JEditorPane htmldisplay = new JEditorPane();
+    //JScrollPane htmlscroll = new JScrollPane(htmldisplay);
+    //htmlscroll.setPreferredSize(new Dimension(300, 500));
 
     // adding elements
-    pane.add(uilabel);
-    pane.add(htmllabel);
+    //pane.add(uilabel);
+    pane.add(bar);
+    //pane.add(htmllabel);
     pane.add(uiscroll);
-    pane.add(bold);
-    pane.add(htmlscroll);
-  }
-
-  public void actionPerformed(ActionEvent e) {
-    // checks if any texts were selected
-    if (ui.getSelectedText() != null) {
-      String boldStart = "<html><strong>";
-      String boldEnd = "</strong></html>";
-      int startSelected = ui.getSelectionStart();
-      int endSelected = ui.getSelectionEnd();
-      ui.selectAll();
-      int afterAll = ui.getSelectionEnd();
-      ui.select(0, startSelected);
-      String beforeSelected = ui.getSelectedText();
-      ui.select(startSelected, endSelected);
-      String selected = ui.getSelectedText();
-      ui.select(endSelected, afterAll);
-      String afterSelected = ui.getSelectedText();
-
-      System.out.println(beforeSelected);
-      System.out.println(selected);
-      System.out.println(afterSelected);
-      if (e.getActionCommand().equals("bold")) {
-        ui.setText(beforeSelected + boldStart + selected + boldEnd + afterSelected);
-        // document.setCharacterAttributes(ui.getSelectionStart(),
-        // ui.getSelectionEnd(),
-        // ui.getStyle("Bold"),
-        // true);
-      }
-    }
+    //pane.add(htmlscroll);
   }
 
   public static void main (String[] args) {
